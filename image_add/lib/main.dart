689dad6a17d141323d4f23ad2277ec26e70bec48
jui1960 <juifull196044@gmail.com>
2025-11-04 22:30:13 +0600

    image add

---
+++ image_add/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+
+        title: Text("image"),
+      ),
+      body:Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            child: Image.asset('asset/images/bpi.png'),
+          )
+
+      )
+    );
+  }
+}
