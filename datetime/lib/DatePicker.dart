b24886e5c402d268f305f825e0f8de941a2fd51a
jui1960 <juifull196044@gmail.com>
2026-06-01 11:32:38 +0600

    datepicker and timepicker

--- datetime/lib/DatePicker.dart
+++ datetime/lib/DatePicker.dart
@@ -7,33 +7,62 @@ void main() {
 class datePicker extends StatelessWidget {
   const datePicker({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Date Picker")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Date picker', style: TextStyle(fontSize: 20)),
-              SizedBox(height: 20),
-              ElevatedButton(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DatepickerScreen(),
+    );
+  }
+}
+
+class datePickerScreen extends StatelessWidget {
+  const datePickerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Date Picker'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Show Date', style: TextStyle(fontSize: 20),),
+
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+
                 onPressed: () async {
-                  DateTime? Datepickerr = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2021),
-                    lastDate: DateTime(2030),
-                  );
-                  if(Datepickerr!= null){
-                    print('SELECT DATE : ${Datepickerr.day}-${Datepickerr.month}-${Datepickerr.year}');
+                  DateTime? datePicked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2021),
+                      lastDate: DateTime(2030));
+
+                  if (datePicked != null) {
+                    print('SELECT DATE: ${datePicked.day} - ${datePicked
+                        .month} - ${datePicked.year}');
                   }
-                },
-                child: Text('Get Date'),
-              ),
-            ],
-          ),
+                }, child: Text('Get Date')),
+            SizedBox(height: 20,),
+            ElevatedButton(onPressed: () async {
+              TimeOfDay? time = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now()
+                  ,
+                  initialEntryMode: TimePickerEntryMode.dial);
+
+              if (time != null) {
+                print('SELECT time : ${time.hour} : ${time.minute}');
+              }
+            }, child: Text('Select time'))
+          ],
         ),
       ),
     );
