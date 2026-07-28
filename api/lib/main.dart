1574a30967373d6ca1461b4ec0191bec80ac9ae5
jui1960 <juifull196044@gmail.com>
2026-07-28 19:15:33 +0600

    update

--- api/lib/main.dart
+++ api/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+//get -> server theke data fetxch kora, all data (https:// domain.com/api/posts), single data (https:// domain.com/api/post?id=1)
+//post -> create a new post (https:// domain.com/api/post)body parameter ar maddhore details pathano hoi
+//patch,put,delete
+
 void main() {
   runApp(const MyApp());
 }
